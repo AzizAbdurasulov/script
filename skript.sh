261e93f2eef7d781273bfa61458ab019d6c2b131
AzizAbdurasulov <132073262+AzizAbdurasulov@users.noreply.github.com>
2023-05-17 11:53:15 -0400

    1

---
+++ skript.sh
@@ -0,0 +1,15 @@
+echo  "AZIZ"
+
+
+mkdir  virus2
+mkdir virus3
+mkdir virus4
+mkdir AZIZ
+mkdir XAXAXAXAXAXAXAXAXAXAXAXAXAXAAXXAAXXAXAAXXAAXAXXAXAXAXAXAAXAXXAAAAAAAAAAAAAAAAAAAAAAAAXAXAXAXAXAXAXAXAXAXAXAXAXAXAXAXAXAXAXAXAXAXAXXAXAXAXAXA
+
+
+cd virussxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxaxax
+
+cd  virus2
+
+touch fayl.sh &fayl.pdf &fayl
